74fa5918eb4bd0567e0517df3ff899f5a750ac1a
21130571 <144263898+21130571@users.noreply.github.com>
2023-10-16 18:39:18 +0700

    this is lab 3

--- AILab/src/chapter2/agent_AB/TestSimpleReflexAgent.java
+++ AILab/src/chapter2/agent_AB/TestSimpleReflexAgent.java
@@ -2,7 +2,7 @@ package chapter2.agent_AB;
 
 public class TestSimpleReflexAgent {
 	public static void main(String[] args) {
-		Environment env = new Environment(Environment.LocationState.CLEAN, Environment.LocationState.DIRTY);
+		Environment env = new Environment(Environment.LocationState.CLEAN, Environment.LocationState.DIRTY,Environment.LocationState.CLEAN,Environment.LocationState.DIRTY);
 		Agent agent = new Agent(new AgentProgram());
 		env.addAgent(agent, Environment.LOCATION_A);
 
